b3422059cc2305a84b23871a1031d241cf91a0bc
AndriiKhavkin <tapusti7@gmail.com>
2023-03-12 19:18:08 +0200

    commit

---
+++ Crypt_bis/Caesar.cs
@@ -0,0 +1,24 @@
+﻿namespace Crypt_bis
+{
+    public class Caesar
+    { 
+        private static char Cipher(char ch, int key) {      
+            if (!char.IsLetter(ch)) {
+                return ch;  //Повертає символ якщо він не є літерою
+            }
+            char d = char.IsUpper(ch) ? 'А' : 'а';  
+            return (char)((((ch + key) - d) % 33) + d);  
+            
+        }
+        public static string Encipher(string input, int key) {  
+            string output = string.Empty;
+            foreach(char ch in input)  
+                output += Cipher(ch, key);
+            return output;  
+        }  
+  
+        public static string Decipher(string input, int key) {  
+            return Encipher(input, 33 - key);  
+        }  
+    }
+}
---
+++ Crypt_bis/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Crypt_bis
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Stopwatch clockCaesar = new Stopwatch();
+            Stopwatch clockGamma = new Stopwatch();
+        
+            Console.WriteLine("Type a string to encrypt:");  
+            string UserString = Console.ReadLine();  
+  
+            Console.WriteLine("\nEnter your Key:");  
+            int key = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("\nGenerating your Key for Gamma Cipher:");
+            string key2 = Gamma.GetKey();
+
+           
+            clockCaesar.Start();
+            string cipherText = Caesar.Encipher(UserString, key);
+            string decryptedText = Caesar.Decipher(cipherText, key); 
+            clockCaesar.Stop();
+            
+            Console.WriteLine("\nEncrypted Data for Caesar\n"); 
+            Console.WriteLine(cipherText);  
+            
+            Console.WriteLine("\nDecrypted Data:\n");
+            Console.WriteLine(decryptedText); 
+            
+            Console.WriteLine("Time spent for caesar cypher it: " + clockCaesar.Elapsed);
+            
+            clockGamma.Start();
+            string cipherText2 = Gamma.GammaCipher(UserString, key2);
+            string decryptedText2 = Gamma.GammaCipher(cipherText2, key2);  
+            clockGamma.Stop();
+            
+            Console.WriteLine("\nEncrypted Data for Gamma\n"); 
+            Console.WriteLine(cipherText2);
+            Console.WriteLine("\nDecrypted Data:\n");
+            Console.WriteLine(decryptedText2); 
+            
+            Console.WriteLine("Time spent for gamma cypher it: " + clockGamma.Elapsed);
+            
+            Console.ReadKey();  
+            Console.ReadKey(); 
+        }
+    }
+}
